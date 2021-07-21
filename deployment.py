33f81951fd8fd2cb67d6c474965df7908935f9bd
divyanshchahar <divyanshchahar@outlook.com>
2021-07-21 20:19:51 +0530

    Added functionality to  extract the name of all attributes and their values
    Earlier the flow of the code was the following steps:
    - extract search id
    - extract urls
    - extract all atttributes from urls
    - create a unique set of attributes
    - extract some features from the urls
    
    This code was inefficient as it was scrapping the same page multiple times
    
    The new code has the following steps
    - extract search id
    - extract urls
    - extract all atttributes and their values from urls

--- deployment.py
+++ deployment.py
@@ -61,84 +61,24 @@ f_all_urls.close()
 now = datetime.now()
 print("URL EXTRACTION COMPLETE  ON", now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
 
-#####################################
-# EXTRACTING ALL FEATURES FROM URLS #
-#####################################
+###################
+# EXTRACTING DATA #
+###################
 
 now = datetime.now()
-print("STARTING FEATURE EXTRACTION",  now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"))
-
-f_all_urls = open("urls.txt", "r")
-content_all_urls = f_all_urls.read() # Reading content form the file
-url_items = content_all_urls.split("\n") # Splitting into different urls by line ending
-url_items.pop(-1) # Removing last line(because it is blank)
-
-f_attributes = open("attributes.txt", "w") # File to write attributes
-
-counter = 1
-
-# Loop to iterate through all urls
-for page_url in url_items:
-
-    f_attributes.write(page_url + "\n") # File to hold attribute information
-
-    code_url = requests.get(page_url.rstrip()).text  # HTML code of the page
-    parsed_code = BeautifulSoup(code_url, "lxml")  # parsed HTML code of the page
-
-    parent_obj = parsed_code.find("div", class_="cldt-item", attrs={"data-item-name":"car-details"})
-
-    # Loop to iterate through all required attributes of a url
-    for j in parent_obj.find_all("dt"):
-        f_attributes.write(j.text + "\n")
-
-    print("Completed ", counter, "out of ", len(url_items), " urls")
-    counter = counter + 1
-
-f_attributes.close()
-
-now = datetime.now()
-print("FEATURE EXTRACTION COMPLETE", now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
-
-###################################
-# CREATING SET OF UNIQUE FEATURES #
-###################################
-
-now = datetime.now()
-print("STARTING CREATION OF SET OF UNIQUE FEATURES AT ", now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
-
-f_attributes = open("attributes.txt", "r")
-
-atrrbts = f_attributes.readlines()
-unique_atrrbts = []
-
-for s in atrrbts:
-    if (s.find("https") == 0):
-        pass
-
-    else:
-        unique_atrrbts.append(s.rstrip())
-
-unique_atrrbts = filter(None, unique_atrrbts)
-
-unique_atrrbts = set(unique_atrrbts)
-
-now = datetime.now()
-print("SUCCESSFULLY CREATED SET OF UNIQUE FEATURES AT ",now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
-
-# print(len(unique_atrrbts)) = 38
-
-##################
-# SCRAPPING DATA #
-##################
+print("DATA SCRAPPING STARTED ON", now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
 
 f_all_urls = open("urls.txt", "r") # File Object for the urls file
 content_all_urls = f_all_urls.read() # Reading content form the file
 url_items = content_all_urls.split("\n") # Splitting into different urls by line ending
 url_items.pop(-1) # Removing last line(because it is blank)
 
+f_attributes_data = open("car_details.txt", "w")
+
 attributes1 = ["price", "car name", "variant", "mileage", "first registration", "horse power"] # List of attributes
 
-temp1 = [] # First list to hold values of attributes
+temp1 = [] # First list to hold values of attributes, same for all page (attributes1)
+temp2 = [] # Second list to hold value of attibutes, different for different pages(attributes2)
 
 # Loop to initialize first list
 for i in range(0, len(attributes1)):
@@ -146,28 +86,33 @@ for i in range(0, len(attributes1)):
 
 counter = 1
 
-# Loop to iterate through urls
+# LOOP TO ITERATE THROUGH URLs
 for page_url in url_items:
 
     print(counter, "out of ", len(url_items))
 
+    f_attributes_data.write(page_url + "\n") # Writing page url to file
+
     code_url = requests.get(page_url).text # HTML code of the page
     parsed_code = BeautifulSoup(code_url, "lxml") # parsed HTML code of the page
 
-    # Extracting price
+    #  EXTRACTING PRICE
+    # parent object for price
     try:
-        parent_obj = parsed_code.find("div", class_="cldt-stage-headline")
+        parent_obj = parsed_code.find("div", class_="cldt-stage-headline") # Parent Object for price
     except:
         print("Unable to extract parent object for price")
 
+    # price
     try:
-        price = parent_obj.find("h2")
-        temp1[0].append(price.text.strip())
+        price = parent_obj.find("h2").text.strip()
     except:
         print("Unable to extract price")
-        temp1[0].append("")
+        price = ""
 
-    # Extracting Vehicle Name and Variant
+    # EXTRACTING VEHICLE NAME AND VARIANT
+
+    # parent object for name and variant
     try:
         parent_obj = parsed_code.find("div", class_="cldt-headline")
     except:
@@ -175,57 +120,90 @@ for page_url in url_items:
 
     # vehicle name
     try:
-        vehicle_name = parent_obj.find("span", class_="cldt-detail-makemodel sc-ellipsis")
-        temp1[1].append(vehicle_name.text)
+        vehicle_name = parent_obj.find("span", class_="cldt-detail-makemodel sc-ellipsis").text
+        f_attributes_data.write("name : " + vehicle_name + "\n")
     except:
         print("Unable to extract vehicle name")
-        temp1[1].append("")
+        f_attributes_data.write("name : " + "\n")
 
     # vehicle variant
     try:
-        vehicle_variant = parent_obj.find("span", class_="cldt-detail-version sc-ellipsis")
-        temp1[2].append(vehicle_variant.text)
+        vehicle_variant = parent_obj.find("span", class_="cldt-detail-version sc-ellipsis").text
+        f_attributes_data.write("variant : " + vehicle_variant + "\n")
     except:
         print("Unable to extract vehicle variant")
-        temp1[2].append("")
+        f_attributes_data.write("variant : " + "\n")
 
-    # Extracting mileage, first registration, horse power
+    f_attributes_data.write("price : " + price + "\n")  # writing price to the file
+
+    # EXTRACTING MILEAGE, FIRST REGISTRATION, HORSE POWER
+
+    # parent object for mileage, first registration and horse power
     try:
         parent_obj = parsed_code.find("div", class_="cldt-stage-basic-data")
     except:
         print("Unable to find parent object")
 
+    # mileage
     try:
         mileage = parent_obj.find("span", class_="sc-font-l cldt-stage-primary-keyfact").text.replace(",", "").split()[0]
-        temp1[3].append(mileage)
+        f_attributes_data.write("mileage : " + mileage + "\n")
     except:
         print("Unable to extract mileage")
-        temp1[3].append("")
+        f_attributes_data.write("mileage : " + "\n")
 
+    # first registration
     try:
         first_registration = parent_obj.find("span", class_="sc-font-l cldt-stage-primary-keyfact", attrs={"id":"basicDataFirstRegistrationValue"}).text
-        temp1[4].append(first_registration)
+        f_attributes_data.write("first registration : " + first_registration + "\n")
     except:
         print("Unable to extract first registration date")
-        temp1[4].append("")
+        f_attributes_data.write("first registration : " + "\n")
 
+    # horse power
     try:
         horse_power = parent_obj.find("span", class_="sc-font-m cldt-stage-primary-keyfact").text.split()[0]
-        temp1[5].append(horse_power)
+        f_attributes_data.write("horse power : " + horse_power + "\n")
     except:
         print("Unable to extract horse power")
-        temp1[5].append("")
+        f_attributes_data.write("horse power : " + "\n")
+
+    # EXTRACTING ATTRIBUTES AND ATTRIBUTE VALUES ON THE PAGE
+
+    try:
+        parent_obj = parsed_code.find("div", class_="cldt-item", attrs={"data-item-name":"car-details"}) # parent object containing all other attributes
+    except:
+        print("Unable to extract parent object for other attributes")
+
+    page_attributes = []
+    page_attribute_features = []
+
+    # Extracting Attributes on page
+    try:
+        for a in parent_obj.find_all("dt"):
+            page_attributes.append(a.text.strip())
+    except:
+        print("Unable to extract attributes")
+
+    # Extracting attribute values from the page
+    try:
+        for v in parent_obj.find_all("dd"):
+            page_attribute_features.append(v.text.strip())
+    except:
+        print("Unable to extract attribute values")
+
+    # writing values in the file
+    if (len(page_attribute_features) == len(page_attributes)):
+
+        for i in range(0, len(page_attributes)):
+            f_attributes_data.write(str(page_attributes[i]) + " : " + str(page_attribute_features[i] + "\n"))
+    else:
+        f_attributes_data.write("FALSE" + "\n")
+        print("Attribues and values have different lenghts" + "\n")
 
     counter = counter + 1
 
-dict1_db = {}
-
-for i in range(0, len(attributes1)):
-    dict1_db[attributes1[i]] = temp1[i]
-
-df_cardetails = pd.DataFrame(dict1_db)
-
-df_cardetails.to_csv("cars.csv", index=False)
+f_attributes_data.close()
 
 now = datetime.now()
-print("EXECUTION COMPLTETD AT", now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
+print("DATA SCRAPPING COMPLETED ON", now.strftime("%d-%B-%Y"), "at", now.strftime("%H:%M:%S"), "\n")
